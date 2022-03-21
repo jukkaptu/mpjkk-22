b06903c309084311ae39422b128d7397307e3634
J-P <jukka-pekka.tuomanen@metropolia.fi>
2022-03-21 08:36:24 +0200

    ajax

--- src/components/MediaTable.js
+++ src/components/MediaTable.js
@@ -1,19 +1,26 @@
-// import PropTypes from 'prop-types';
-import {useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
+//  import PropTypes from 'prop-types';
 import MediaRow from './MediaRow';
 
 const MediaTable = () => {
   const [mediaArray, setMediaArray] = useState([]);
   const getMedia = async () => {
     try {
-      const response = await fetch('test.json');
-      const json = await response.json();
-      setMediaArray(json);
-    } catch (err) {
-      alert(err.message);
+      const mediaResponse = await fetch(baseUrl + 'media');
+      const media = await mediaResponse.json();
+      const allFiles = await Promise.all(
+        media.map(async (file) => {
+          const fileResponse = await fetch(`${baseUrl}media/${file.file_id}`);
+          const json = await fileResponse.json();
+          return json;
+        })
+      );
+      setMediaArray(allFiles);
+    } catch (e) {
+      console.log(e.message);
     }
   };
-
   useEffect(() => {
     getMedia();
   }, []);
